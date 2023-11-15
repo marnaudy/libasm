882aede2edf2712dab0f9f5a13594d700e5be412
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2023-11-15 13:56:31 +0100

    Hello world with strlen

--- hello_world.asm
+++ hello_world.asm
@@ -1,15 +1,25 @@
 section .data
-msg	db 'hello world', 0xA
+msg	db 'hello world!!!', 0xA, 0
 
 section	.text
 global	_start
 
 _start:
-	mov eax, 1
+	mov rax, msg
+	mov rbx, msg
+loop:
+	cmp byte [rbx], 0
+	jz loop_end
+	inc rbx
+	jmp loop
+loop_end:
+	sub rbx, rax; ebx now contains length of message
+
+	mov rax, 1
 	mov rdi, 1
 	mov rsi, msg
-	mov rdx, 12
+	mov rdx, rbx
 	syscall
-	mov eax, 0x3c
+	mov rax, 0x3c
 	mov rdi, 0
 	syscall
