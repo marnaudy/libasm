85a21c33792ac1c9f8d358f914b8dae8a0c9b59e
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2023-11-15 14:16:36 +0100

    Hello world with strlen function (called, not jumped)

--- hello_world.asm
+++ hello_world.asm
@@ -5,21 +5,28 @@ section	.text
 global	_start
 
 _start:
-	mov rax, msg
-	mov rbx, msg
-loop:
-	cmp byte [rbx], 0
-	jz loop_end
-	inc rbx
-	jmp loop
-loop_end:
-	sub rbx, rax; ebx now contains length of message
+	mov		rax, msg
+	call	strlen
+	mov		rdx, rax; len 
+	mov		rax, 1; write
+	mov		rdi, 1; fd
+	mov		rsi, msg; str pointer
+	syscall
+	mov		rax, 0x3c; exit
+	mov		rdi, 0; return
+	syscall
 
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, rbx
-	syscall
-	mov rax, 0x3c
-	mov rdi, 0
-	syscall
+strlen:
+	push	rbx
+	mov		rbx, rax
+
+loop:
+	cmp		byte [rax], 0
+	jz		loop_end
+	inc		rax
+	jmp		loop
+
+loop_end:
+	sub		rax, rbx; rax now contains length of message
+	pop		rbx
+	ret
