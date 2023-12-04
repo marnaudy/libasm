aa7133e258b68fabb19be123e0027fe3490d5819
Maxime Arnaudy <maxime.arnaudy@gmail.com>
2023-12-05 00:25:46 +0100

    Separate util functions to separate file

--- hello_world.asm
+++ hello_world.asm
@@ -1,32 +1,13 @@
-section .data
-msg	db 'hello world!!!', 0xA, 0
+%include 'utils.asm'
 
-section	.text
-global	_start
+section .data
+msg	db      'hello world!!!', 0
+
+section     .text
+global      _start
 
 _start:
-	mov		rax, msg
-	call	strlen
-	mov		rdx, rax; len 
-	mov		rax, 1; write
-	mov		rdi, 1; fd
-	mov		rsi, msg; str pointer
-	syscall
-	mov		rax, 0x3c; exit
-	mov		rdi, 0; return
-	syscall
-
-strlen:
-	push	rbx
-	mov		rbx, rax
-
-loop:
-	cmp		byte [rax], 0
-	jz		loop_end
-	inc		rax
-	jmp		loop
-
-loop_end:
-	sub		rax, rbx; rax now contains length of message
-	pop		rbx
-	ret
+    mov     rax, msg
+    call    print_nl
+    mov     rax, 0
+    call    exit
