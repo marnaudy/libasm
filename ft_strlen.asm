69f74bd67241acc64e9939d048561f139f60f101
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2024-01-17 10:51:57 +0100

    ft_strlen improve comments

--- ft_strlen.asm
+++ ft_strlen.asm
@@ -1,21 +1,32 @@
 ;--------------------------------------------------
-; ft_strlen(string str)
+; int ft_strlen(char *str)
 ; Calculates length of str
+; str in rdi
+; return in rax
+
 section    .text
 global     ft_strlen
 
 ft_strlen:
+    ; Push rbx on stack
     push    rbx
+    ; Move str to rax and rbx
     mov     rbx, rdi
     mov     rax, rdi
 
 next_char:
-    cmp     byte [rax], 0   ; check if byte at rax is null (end of string)
+    ; Check if byte at rax is null (end of string)
+    cmp     byte [rax], 0
+    ; If null jump to end
     jz      finished
+    ; Else go to next character
     inc     rax
     jmp     next_char
 
 finished:
-    sub     rax, rbx        ; calculate length as difference in address of start and null
+    ; Calculate length as difference in address of start and null
+    ; Store in rax for return
+    sub     rax, rbx
+    ; Restore rbx
     pop     rbx
     ret
