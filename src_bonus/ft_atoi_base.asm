3c960938eaff6266e4a0df398d6a4a97b89fdd2c
Maxime Arnaudy <maxime.arnaudy@gmail.com>
2024-04-28 19:40:52 +0200

    Add testing for atoi_base

--- src_bonus/ft_atoi_base.asm
+++ src_bonus/ft_atoi_base.asm
@@ -19,4 +19,5 @@ section     .text
 global      ft_atoi_base
 
 ft_atoi_base:
+    xor     rax, rax
     ret
--- test_src/test_atoi_base.c
+++ test_src/test_atoi_base.c
@@ -3,5 +3,69 @@
 void    test_atoi_base() {
     puts("----- Testing atoi_base");
     int test_nbr = 1;
-    (void) test_nbr;
+    char *base10 = "0123456789";
+    char *base16 = "0123456789abcdef";
+    
+    //Invalid base
+    test_int(&test_nbr, ft_atoi_base("10", ""), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "1"), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "010"), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "01+"), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "01-"), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "01 "), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "01\f"), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "01\t"), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "01\n"), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "01\r"), 0);
+    test_int(&test_nbr, ft_atoi_base("10", "01\v"), 0);
+    
+    //Basic positive formatting
+    test_int(&test_nbr, ft_atoi_base("", base10), 0);
+    test_int(&test_nbr, ft_atoi_base("0", base10), 0);
+    test_int(&test_nbr, ft_atoi_base("1", base10), 1);
+    test_int(&test_nbr, ft_atoi_base("1234567890", base10), 1234567890);
+
+    //Formatting with signs
+    test_int(&test_nbr, ft_atoi_base("+", base10), 0);
+    test_int(&test_nbr, ft_atoi_base("-", base10), 0);
+    test_int(&test_nbr, ft_atoi_base("-0", base10), 0);
+    test_int(&test_nbr, ft_atoi_base("+-0", base10), 0);
+    test_int(&test_nbr, ft_atoi_base("-1", base10), -1);
+    test_int(&test_nbr, ft_atoi_base("++++-1", base10), -1);
+    test_int(&test_nbr, ft_atoi_base("-+-+1", base10), 1);
+    test_int(&test_nbr, ft_atoi_base("-+-+-+1", base10), -1);
+    test_int(&test_nbr, ft_atoi_base("+-+1234567890", base10), -1234567890);
+    test_int(&test_nbr, ft_atoi_base("+-+-+1234567890", base10), 1234567890);
+
+    //Formatting with whitespace
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n", base10), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n0", base10), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n1", base10), 1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n1234567890", base10), 1234567890);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n+", base10), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-", base10), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-0", base10), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n+-0", base10), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-1", base10), -1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n++++-1", base10), -1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-+-+1", base10), 1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-+-+-+1", base10), -1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n+-+1234567890", base10), -1234567890);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n+-+-+1234567890", base10), 1234567890);
+
+    //Formatting with trailing characters
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n+aa", base16), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n0+aa", base16), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n1+aa", base16), 1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \nffff+aa", base16), 65535);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n+Aaa", base16), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-Aaa", base16), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-0Aaa", base16), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n+-0Aaa", base16), 0);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-1Aaa", base16), -1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n++++-1 aaa", base16), -1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-+-+1 aaa", base16), 1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n-+-+-+1 aaa", base16), -1);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n+-+ffff aaa", base16), -65535);
+    test_int(&test_nbr, ft_atoi_base(" \f\t\n\r\v \n+-+-+ffff aaa", base16), 65535);
 }
