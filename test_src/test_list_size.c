b2540774193d18277923e52e75cc55dcb0792ec8
Maxime Arnaudy <maxime.arnaudy@gmail.com>
2024-04-28 17:02:34 +0200

    Add testing for list_size

--- test_src/test_list_size.c
+++ test_src/test_list_size.c
@@ -3,5 +3,19 @@
 void    test_list_size() {
     puts("----- Testing list_size");
     int test_nbr = 1;
-    (void) test_nbr;
+    t_list *list_begin = NULL;
+    char *str = "Hello";
+
+    //Testing with empty list
+    test_int(&test_nbr, ft_list_size(list_begin), 0);
+
+    //Testing size 1
+    list_push_front(&list_begin, str);
+    test_int(&test_nbr, ft_list_size(list_begin), 1);
+    free(list_begin);
+
+    //Testing size 10
+    list_begin = create_list();
+    test_int(&test_nbr, ft_list_size(list_begin), 10);
+    free_list(list_begin);
 }
